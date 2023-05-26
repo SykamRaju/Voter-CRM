a1f34b17ed5aa2595d8cd5a19ab120fb8993f949
Sykam Raju <www.raju@gmail.com>
2023-05-27 00:38:54 +0530

    Constituency
    Constituency

--- Front-end Streamlit/Views/AddConstituency.py
+++ Front-end Streamlit/Views/AddConstituency.py
@@ -3,15 +3,16 @@ from typing import Callable
 
 
 class AddConstituency:
-    def __init__(self, on_submit: Callable[[str, str], bool]):
+    def __init__(self,get_districts: Callable[[str], bool], on_submit: Callable[[str, str], bool]):
         st.header("Add New Constituency")
-
+        districts=get_districts()
+        district_names = [district["district_name"] for district in districts]
         form = st.form("new_constituency")
+        selected_district = form.selectbox("Select a District", district_names)
         constituency_name = form.text_input("Constituency name")
-        # country = form.text_input("Country")
 
         if form.form_submit_button("Add New Constituency"):
-            success = on_submit(constituency_name)
+            success = on_submit(selected_district,constituency_name)
             if success:
                 st.success("Constituency Added Successfully")
             else:
